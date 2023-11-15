f16ff54ccb02d2a396e5f323fc0519d8668015ae
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-14 18:56:51 -0800

    Added GET-ME query to queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,24 @@
+import {gql} from '@apollo/client';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }  
+`;
+
+export const searchGoogleBooks = (query) => {
+  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
+
