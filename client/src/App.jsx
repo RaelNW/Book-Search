2e53095cd50479e9f47ed89514e398e4b97f66d1
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-14 18:51:29 -0800

    Added the mutations.js and queries.js and added code to the mutations.js

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import{ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+const
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
