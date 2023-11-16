3bcaf700f502931fe3f34b9cd8988fd3ac57b1e5
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-16 15:54:59 -0800

    Added type defts

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
+ 
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -26,28 +30,36 @@ const SignupForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-
-    try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      try{
+        const {data} = await addUser({
+          variables: {...userFormData}
+        });
+        Auth.login(data.addUser.token);
+      } catch (e) {
+        console.error(e);
       }
+    };
+  //   try {
+  //     const response = await createUser(userFormData);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+  //     if (!response.ok) {
+  //       throw new Error('something went wrong!');
+  //     }
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+  //     const { token, user } = await response.json();
+  //     console.log(user);
+  //     Auth.login(token);
+  //   } catch (err) {
+  //     console.error(err);
+  //     setShowAlert(true);
+  //   }
+
+  //   setUserFormData({
+  //     username: '',
+  //     email: '',
+  //     password: '',
+  //   });
+  // };
 
   return (
     <>
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,41 @@
 const typeDefs =
 // Path: server/schemas/typeDefs.js
+`type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Query {
+    me: User
+}
+input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
 
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
